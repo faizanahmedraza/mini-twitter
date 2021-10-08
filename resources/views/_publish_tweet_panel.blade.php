cbbcfb933d40f7f3b7c0bb476727d144614a4fc1
unknown <fahmed.raza@viftech.com.pk>
2021-10-08 10:31:44 +0500

    add changes

--- resources/views/_publish_tweet_panel.blade.php
+++ resources/views/_publish_tweet_panel.blade.php
@@ -1,4 +1,45 @@
-<div class="px-5 py-3 border-b border-gray-200">
+@push('styles')
+    <style>
+        .image-upload > input {
+            display: none;
+        }
+
+        .yes {
+            position: relative;
+            display: flex;
+            align-items: flex-start;
+            margin-top: 10px !important;
+        }
+
+        .it {
+            height: 100px;
+            margin-left: 10px;
+        }
+
+        .btn-rmv1 {
+            display: none;
+        }
+
+        .rmv {
+            cursor: pointer;
+            color: #fff;
+            border-radius: 50%;
+            border: 1px solid #fff;
+            display: inline-block;
+            background: #39383880;
+            margin: -5px -10px;
+            padding: 0px 7px;
+        }
+
+        .rmv:hover {
+            background: #10101080;
+        }
+
+
+    </style>
+@endpush
+
+<div class="px-5 py-3 border-b border-t border-gray-200">
     <div class="flex">
         <div class="mr-4 flex-shrink-0">
             <img src="{{current_user()->avatar}}"
@@ -12,16 +53,22 @@
             @csrf
             <textarea name="body" id="" class="w-full placeholder-gray-600 border-0 focus:outline-none text-lg"
                       placeholder="What's Happening?" autofocus required></textarea>
+            <div class="yes">
+                <img id="ImgPreview" src="" class="preview1"/>
+                <input type="button" id="removeImage1" value="x" class="btn-rmv1"/>
+            </div>
             @error('body')
             <p class="text-red-400 text-sm mt-2">{{$message}}</p>
             @enderror
             <hr class="my-2"/>
 
             <footer class="flex justify-between items-center">
-                <div class="text-blue-400 lg:pt-2 text-2xl">
-                    <div class="fileUploadWrap">
-                        <div class="flex-initial pr-2"><i class="fal fa-smile fa-fw"></i></div>
-                        <input id="" accept="image/jpeg,image/png,image/webp" tabindex="-1" type="file" class="" data-testid="fileInput">
+                <div class="flex justify-start text-blue-400 lg:pt-2 text-2xl">
+                    <div class="image-upload">
+                        <label for="file-input">
+                            <i class="fal fa-image fa-fw" style="font-size: 26px; margin-top: 5px; cursor: pointer;"></i>
+                        </label>
+                        <input id="file-input" type="file"/>
                     </div>
                     <a class="flex-initial" href=""><i class="fal fa-calendar-check fa-fw"></i></a>
                 </div>
@@ -32,3 +79,32 @@
         </form>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        function readURL(input, imgControlName) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $(imgControlName).attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#file-input").change(function () {
+            var imgControlName = "#ImgPreview";
+            readURL(this, imgControlName);
+            $('.preview1').addClass('it');
+            $('#removeImage1').addClass('rmv');
+        });
+
+        $("#removeImage1").click(function (e) {
+            e.preventDefault();
+            $("#file-input").val("");
+            $("#ImgPreview").attr("src", "");
+            $('.preview1').removeClass('it');
+            $(this).removeClass('rmv');
+        });
+    </script>
+@endpush
